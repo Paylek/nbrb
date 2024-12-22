7a8981ead2a538cefa53aa1b2ca36c47817ff6a0
Pavel <pggsviryd@gmail.com>
2024-12-22 12:59:50 +0300

    First commit

---
+++ src/test/java/com/sviryd/nbrb/NbrbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sviryd.nbrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbrbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
